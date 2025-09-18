ace25b8dd44749293ad5ce9c7997ae6d3e563198
hzquare12 <hzquare12@gmail.com>
2025-09-18 21:35:46 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
 
   // Windows uyumlu listen
-  server.listen(port, "127.0.0.1", () => {
-    log(`Server running on http://127.0.0.1:${port}`);
+  server.listen(port, "0.0.0.0", () => {
+    log(`Server running on http://0.0.0.0:${port}`);
   });
 })();
